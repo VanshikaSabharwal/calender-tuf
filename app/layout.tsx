b28626e84d6f9bda80c1884ed4df203a24a34ab0
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2026-04-08 15:35:19 +0530

    upgraded to latest next version

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import type { Metadata, Viewport } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Wall Calendar',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
+type RootLayoutProps = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
--- components/Calendar.js
+++ components/Calendar.js
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect, useRef } from 'react'
 import styles from './Calendar.module.css'
 import SpiralBinding from './SpiralBinding'
